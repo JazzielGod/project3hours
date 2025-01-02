802dcb95114857958fa077c6d8a358efb3a5677a
JazzielGod <jazzielbh@gmail.com>
2025-01-01 17:29:56 -0700

    add: activation links

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,3 +3,12 @@ import '../css/app.scss';
 // import * as bootstrap from 'bootstrap';
 
 console.log('Hello World');
+
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.querySelector('[data-collapse-toggle]');
+    const navbar = document.getElementById('navbar-default');
+
+    toggleButton.addEventListener('click', function () {
+        navbar.classList.toggle('hidden');
+    });
+});
